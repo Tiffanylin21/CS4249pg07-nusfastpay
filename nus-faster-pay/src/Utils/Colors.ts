2fd78c7237c8f1d350b7d349678e061eb238bc19
Lin Hui Xin Tiffany <tiffanylinnn21@gmail.com>
2024-03-18 13:51:22 +0800

    feat: add shared components

---
+++ nus-faster-pay/src/Utils/Colors.ts
@@ -0,0 +1,16 @@
+// TO BE EDITED We edit this to standardise the colors we are using!!
+
+export const COLORS = {
+    primary: "#FFFFFF",
+    secondary: "#FFFFFF",
+    tertiary: "#FFFFFF",
+    black: "#000000",
+    white: "#FFFFFF",
+    gray: "#FFFFFF",
+    lightGray: "#FFFFFF",
+    darkGray: "#FFFFFF",
+    error: "#FFFFFF",
+    success: "#FFFFFF",
+    warning: "#FFFFFF",
+    info: "#FFFFFF"
+};
---
+++ nus-faster-pay/src/Utils/Data.ts
@@ -0,0 +1,25 @@
+import { PaymentItem } from "./Types";
+
+export const paymentItems: PaymentItem[] = [
+  {
+    title: "Education Records System Tuition Fees",
+    description: "AY23/24 Semester 1 Tuition Fees",
+    price: 6287,
+    quantity: 1,
+    deadline: "2024-01-30",
+  },
+  {
+    title: "Student Housing Fees",
+    description: "AY23/24 Semester 1 Housing Fees",
+    price: 1000,
+    quantity: 1,
+    deadline: "2024-01-30",
+  },
+  {
+    title: "NUS Mug",
+    description: "NUS Mug",
+    price: 10,
+    quantity: 1,
+    deadline: "2024-02-20",
+  },
+];
