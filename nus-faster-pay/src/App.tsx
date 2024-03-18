f010a7abb04b73a8cfaa60b8f08d434238a762c3
Lin Hui Xin Tiffany <tiffanylinnn21@gmail.com>
2024-03-18 13:52:32 +0800

    feat: update routes

--- nus-faster-pay/src/App.tsx
+++ nus-faster-pay/src/App.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import StartScreen from "./Screens/StartScreen";
 import "./App.css";
-import MyPayments from "./Screens/FasterPay/MyPayments";
-import PaymentOptions from "./Screens/FasterPay/PaymentOptions";
-import QRCode from "./Screens/FasterPay/QRCode";
+import PaymentOptions from "./Screens/PaymentOptions";
+import QRCode from "./Screens/QRCode";
+import AccountDashbaord from "./Screens/AccountDashboard";
+import ShoppingCart from "./Screens/ShoppingCart";
+import CustomerDetails from "./Screens/CustomerDetails";
 
 function App() {  
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
-      <Route path="/my-payments" element={<MyPayments />} />
+      <Route path="/account-dashboard" element={<AccountDashbaord />} />
+      <Route path="/shopping-cart" element={<ShoppingCart />} />
       <Route path="/payment-options" element={<PaymentOptions />} />
+      <Route path="/customer-details" element={<CustomerDetails />} />
       <Route path="/qr-code" element={<QRCode />} />
     </Routes>
   );
